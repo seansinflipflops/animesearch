117305936c5eac092c7cfb9d1b4fa36f47998d82
Mu <zkfire2345@gmail.com>
2016-11-29 11:26:08 -0500

    css

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
+var slug = require("slug");
 
 app.use(express.static(__dirname + '/../app'))
 app.use(bodyParser.json())
 
 app.post('/anime', function(req, resp) {
-	request.get('https://hummingbird.me/api/v1/anime/' + req.body.anime, function(error, res, body) {
+	request.get('https://hummingbird.me/api/v1/anime/' + slug(req.body.anime), function(error, res, body) {
 		console.log(body);
 		resp.json(body);
 	});
