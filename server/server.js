d5388e779e700f4f57fa0aeb80274abd959c6825
Mu <zkfire2345@gmail.com>
2016-11-29 13:30:21 -0500

    implemented toLowerCase

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/../app'))
 app.use(bodyParser.json())
 
 app.post('/anime', function(req, resp) {
-	request.get('https://hummingbird.me/api/v1/anime/' + slug(req.body.anime), function(error, res, body) {
+	request.get('https://hummingbird.me/api/v1/anime/' + slug(req.body.anime).toLowerCase(), function(error, res, body) {
 		console.log(body);
 		resp.json(body);
 	});
