1436bbbddd7b139395e277e310c7c1d7afe041ec
Mu <zkfire2345@gmail.com>
2016-11-29 14:15:22 -0500

    added synopsis, changed app to info search instead

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ var app = angular.module('animeSearch', []);
 
 app.controller('searchCtrl', function($scope, $http) {
 	$scope.anime = {};
-
 	$scope.animeCatcher = function(searchTitle) {
 		$http({
 			method: 'POST',
@@ -14,6 +13,7 @@ app.controller('searchCtrl', function($scope, $http) {
 				$scope.anime.title = prsed.title;
 				$scope.anime.image = prsed.cover_image;
 				$scope.anime.url = prsed.url;
+				$scope.anime.synopsis = prsed.synopsis;
 			})
 			.catch(function(error) {
 				console.log(error);
